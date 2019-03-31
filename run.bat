d77a85f9eeb0f176279bce25bfc0a67e3240d402
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2019-03-31 16:32:23 +0100

    Added scripts for running the program

---
+++ run.bat
@@ -0,0 +1,4 @@
+set CLASS-PATH=".\out\production\IART1819_T5"
+set MAIN="core.LabyrinthRobot"
+
+java --class-path %CLASS-PATH% %MAIN%
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+CLASS_PATH=".\out\production\IART1819_T5"
+MAIN="core.LabyrinthRobot"
+
+java --class-path $CLASS_PATH $MAIN
