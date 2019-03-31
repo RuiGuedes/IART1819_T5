eab4c9fc8913d98f5be51ba4b7c3fc46ec23103c
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2019-03-31 19:18:04 +0100

    Added no close to scripts

--- run.bat
+++ run.bat
@@ -2,3 +2,4 @@ set CLASS-PATH=".\out\production\IART1819_T5"
 set MAIN="core.LabyrinthRobot"
 
 java --class-path %CLASS-PATH% %MAIN%
+cmd /k
--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ CLASS_PATH=".\out\production\IART1819_T5"
 MAIN="core.LabyrinthRobot"
 
 java --class-path $CLASS_PATH $MAIN
+$SHELL
